e6f315b72905db551bfaa512d1d570ee47fd8e45
Sherif Darwish <48363804+ShrifDarwish7@users.noreply.github.com>
2019-12-24 20:08:20 +0200

    Initial Commit

---
+++ The RepoTests/The_RepoTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_RepoTests.swift
+//  The RepoTests
+//
+//  Created by Sherif Darwish on 12/24/19.
+//  Copyright © 2019 Sherif Darwish. All rights reserved.
+//
+
+import XCTest
+@testable import The_Repo
+
+class The_RepoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

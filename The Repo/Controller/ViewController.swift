bce78acfb1e84057ca4a1d6377ebb1a71dbc4bf4
Sherif Darwish <48363804+ShrifDarwish7@users.noreply.github.com>
2020-01-04 09:51:06 +0200

    last commit

--- The Repo/Controller/ViewController.swift
+++ The Repo/Controller/ViewController.swift
@@ -13,7 +13,7 @@ import Lottie
 import ProgressHUD
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     var refreshControl : UIRefreshControl?
@@ -26,8 +26,10 @@ class ViewController: UIViewController {
     var page = 1
     var per_page = 10
     
+    var searching = false
+    
     let RepoCache = NSCache<NSString, ReposHolder>()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -41,38 +43,28 @@ class ViewController: UIViewController {
         loadMoreDeclartion()
         addRefreshControl()
         
-          NotificationCenter.default.addObserver(self, selector: #selector(self.reloadData(_:)), name: NSNotification.Name("ReloadNotification"), object: nil)
-  
+        NotificationCenter.default.addObserver(self, selector: #selector(self.reloadData(_:)), name: NSNotification.Name("ReloadNotification"), object: nil)
+        
+        self.definesPresentationContext = true
     }
     
-    ////////////// /////////////////////////////
-    ///////Mark :- Setup The View
     
+// MARK: - View Initialization
     
     func setupSearchBarAndLoading (){
-    navigationController?.navigationBar.prefersLargeTitles = true
-    searchController = UISearchController(searchResultsController: nil)
-    searchController!.searchBar.delegate = self
-    searchController!.searchResultsUpdater = self
-    navigationItem.searchController = searchController
-    navigationItem.hidesSearchBarWhenScrolling = false
-        loading.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        searchController = UISearchController(searchResultsController: nil)
+        searchController!.searchBar.delegate = self
+        searchController!.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController?.obscuresBackgroundDuringPresentation = false
+        loading.frame = CGRect(x: 0, y: 0, width: 100, height: 65)
         loading.sizeToFit()
     }
     
-    
-    func loadMoreDeclartion(){
-        loadMore = UIButton(frame: CGRect.init(x: 0, y: 0, width: tableView.bounds.width, height: 50))
-        loadMore?.setTitle("Load More", for: .normal)
-        loadMore?.setTitleColor(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1), for: .normal)
-        loadMore?.addTarget(self, action: #selector(self.loadMoreItems), for: .touchUpInside)
-    }
-    
-    
-    ////////////////////////////////////////////
-    /////Mark :- notification Functions
-    
     override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         self.tableView.reloadData()
     }
     
@@ -83,7 +75,9 @@ class ViewController: UIViewController {
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-
+    
+    // MARK: - Notification Pressed Function
+    
     func NotificationPressed(){
         AlamofireRequest(page: 1, per_page: 10) { (Result) in
             self.Repos = Result
@@ -91,9 +85,8 @@ class ViewController: UIViewController {
             NotificationCenter.default.post(name: NSNotification.Name("ReloadNotification"), object: nil)
         }
     }
-
-    ////////////////////////////////////////////////////////
-    ///////////Mark :- Pull to Refresh and infinete scrollingFunctions
+    
+// Mark: - Pull To Refresh ( UIRefreshController )
     
     func addRefreshControl(){
         refreshControl = UIRefreshControl()
@@ -101,7 +94,7 @@ class ViewController: UIViewController {
         refreshControl?.addTarget(self, action: #selector(self.refreshData), for: .valueChanged)
         tableView.addSubview(refreshControl!)
     }
-
+    
     @objc func refreshData(){
         AlamofireRequest(page: 1, per_page: 10) { (result) in
             self.page = 1
@@ -114,10 +107,19 @@ class ViewController: UIViewController {
             self.page += 1
         }
         print(Repos.count)
-            self.tableView.reloadData()
+        self.tableView.reloadData()
         
     }
-    ///////////
+    
+// MARK: - Load More Items Button ( Infinite Scrolling )
+    
+    func loadMoreDeclartion(){
+        loadMore = UIButton(frame: CGRect.init(x: 0, y: 0, width: tableView.bounds.width, height: 50))
+        loadMore?.setTitle("Load More", for: .normal)
+        loadMore?.setTitleColor(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1), for: .normal)
+        loadMore?.addTarget(self, action: #selector(self.loadMoreItems), for: .touchUpInside)
+    }
+    
     @objc func loadMoreItems(){
         tableView.tableFooterView = loading
         tableView.tableFooterView?.isHidden = false
@@ -126,8 +128,7 @@ class ViewController: UIViewController {
         RetriveOrFetch(page: page)
     }
     
-    ////////////////////////////////////////////////////////
-    ///////////Mark : Cahceing Functions and Holder
+// MARK: - Cahceing Stuff & Holder
     
     class ReposHolder: NSObject {
         let pageofRepos: [RepoElement]
@@ -138,10 +139,12 @@ class ViewController: UIViewController {
     
     func CachingPageOfRepos( PageOfRepos : [RepoElement] ){
         RepoCache.setObject(ReposHolder(pageofRepos: PageOfRepos), forKey: "\(self.page)" as NSString)
-        }
+        print("page \(page) is cached")
+    }
     
     func RetrivingPageFromRepos (page : Int) -> [RepoElement] {
         if let RetrivedPageOfRepos = RepoCache.object(forKey: "\(page)" as NSString){
+            print("retriving page \(page)")
             return RetrivedPageOfRepos.pageofRepos
         }else{
             print("PageNotExsit")
@@ -152,9 +155,31 @@ class ViewController: UIViewController {
     func RemoveAllCache(){
         self.RepoCache.removeAllObjects()
     }
-
-    ////////////////////////////////////////////////////////
-    ///////////Mark : AlamoFireRequests
+    
+    func RetriveOrFetch(page : Int){
+        let RetrivedPage = RetrivingPageFromRepos(page: page)
+        if RetrivedPage.isEmpty {
+            AlamofireRequest(page: page , per_page: per_page) { (result) in
+                for temp in result {
+                    self.Repos.append(temp)
+                }
+                ProgressHUD.dismiss()
+                self.page += 1
+                self.tableView.reloadData()
+            }
+        }else{
+            print("if else fetch or retive is in retrive now")
+            for NextRepo in RetrivedPage {
+                self.Repos.append(NextRepo)
+                print("retriving page \(page)")
+            }
+            self.page += 1
+            self.tableView.reloadData()
+            ProgressHUD.dismiss()
+        }
+    }
+    
+// MARK: - Alamofire Request
     
     func AlamofireRequest(page : Int , per_page : Int , completion : @escaping ([RepoElement])->Void){
         let URL = "https://api.github.com/users/square/repos?page=\(page)&per_page=\(per_page)"
@@ -168,50 +193,31 @@ class ViewController: UIViewController {
                     var result = [RepoElement]()
                     for repository in repositories! {
                         result.append(repository)
-                       }
+                    }
                     self.CachingPageOfRepos(PageOfRepos: result)
                     completion(result)
                 } catch {}
             case .failure(let error):
                 print("Failaure \(error.localizedDescription)")
-                 self.showAlert(title: "Faild", message: "Sorry Please Try Again Later")
-                }
+                ProgressHUD.dismiss()
+                self.showAlert(title: "Failed", message: "Sorry Please Try Again Later")
+                self.tableView.tableFooterView = self.loadMore
             }
+        }
         ProgressHUD.dismiss()
     }
-
-    func RetriveOrFetch(page : Int){
-        let RetrivedPage = RetrivingPageFromRepos(page: page)
-        if RetrivedPage.isEmpty {
-            AlamofireRequest(page: page , per_page: per_page) { (result) in
-                for temp in result {
-                    self.Repos.append(temp)
-                }
-                ProgressHUD.dismiss()
-                self.page += 1
-                self.tableView.reloadData()
-            }
-        }else{
-            for NextRepo in RetrivedPage {
-                self.Repos.append(NextRepo)
-            }
-            self.page += 1
-            ProgressHUD.dismiss()
-        }
-    }
     
-}////End of the class
+}
 
 
-/////////////////////////////////////////////////////////////////
-///MARK : TableView Functions
+// MARK: - TableView Delegate Functions
 
 extension ViewController : UITableViewDelegate , UITableViewDataSource{
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if filteredRepos.isEmpty == true{
+        if searching == false{
             return Repos.count
         }else{
             return filteredRepos.count
@@ -221,7 +227,7 @@ extension ViewController : UITableViewDelegate , UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RepoCell", for: indexPath) as? RepoTableViewCell
-        if filteredRepos.isEmpty == true{
+        if searching == false{
             print(Repos.count)
             let  NextRepo = self.Repos[indexPath.row]
             cell!.ConfigureCell(RepoName: NextRepo.name ?? "name", RepoDesc: NextRepo.repoDescription ?? "desc", OwnerName: (NextRepo.owner?.login).map { $0.rawValue } ?? "" , OwnerAvatar: (NextRepo.owner?.avatarURL)! )
@@ -258,28 +264,71 @@ extension ViewController : UITableViewDelegate , UITableViewDataSource{
         let goToOwner = UIContextualAction(style: .normal, title: "Go To Owner") { (action, _, _) in
             UIApplication.shared.open(URL(string: (self.Repos[indexPath.row].owner?.htmlURL)!)!, completionHandler: nil)
         }
+        
         goToRepository.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
         goToOwner.backgroundColor = #colorLiteral(red: 0, green: 0.9914394021, blue: 1, alpha: 1)
         let configuration = UISwipeActionsConfiguration(actions: [goToRepository , goToOwner])
+        configuration.performsFirstActionWithFullSwipe = false
         return configuration
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let swipeAction = UISwipeActionsConfiguration(actions: [])
+        swipeAction.performsFirstActionWithFullSwipe = false
+        return swipeAction
     }
     
 }
 
-/////////////////////////////////////////////////////////////////
-///MARK : Search Functions
+// MARK: - SearchBar Delegate Functions
 
 extension ViewController : UISearchResultsUpdating , UISearchBarDelegate{
+    
     func updateSearchResults(for searchController: UISearchController) {
+        
     }
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    
+        guard searchController!.searchBar.text?.count != 0 else {
+            return
+        }
+        searching = true
         tableView.tableFooterView?.isHidden = true
-        self.filteredRepos = Repos.filter({($0.name?.lowercased().contains(searchText.lowercased()))!})
-        tableView.reloadData()
+        refreshControl?.removeFromSuperview()
+        search(searchBar: searchBar)
+        tableView.scrollToRow(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
     }
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searching = false
+        tableView.addSubview(refreshControl!)
         filteredRepos.removeAll()
-        tableView.reloadData()
+        tableView.tableFooterView?.isHidden = false
+        showTable()
+        searchBar.endEditing(true)
     }
-
+    
+    func showTable(){
+        tableView.reloadData()
+        tableView.isHidden = false
+    }
+    
+// MARK: - Search Function
+    
+    func search(searchBar : UISearchBar){
+        if searchBar.text == "" {
+            searching = false
+            showTable()
+        }else{
+            self.filteredRepos = Repos.filter({ ($0.name?.lowercased().prefix(searchBar.text!.count).contains(searchBar.text!.lowercased()))! })
+            guard filteredRepos.isEmpty != true , filteredRepos.count != 0 else {
+                tableView.isHidden = true
+                refreshControl?.removeFromSuperview()
+                return
+            }
+            showTable()
+        }
+    }
+    
 }
